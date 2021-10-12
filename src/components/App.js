e2e65d02e9c4e89ba788be9ea054ca0e07c34917
IreGonzalez <irenegonzalezpinilla@gmail.com>
2021-10-12 19:58:27 +0200

    done basic html

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,110 @@ import '../styles/App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hola Mundo</h1>
+      <nav className="nav">
+        <button className="nav__button">Fridge</button>
+        <h1 className="nav__button">Lista de la compra</h1>
+        <button className="nav__button">Planning</button>
+      </nav>
+      <main className="main">
+        <section className="section__shopping">
+          <ul className="shopping">
+            <li className="shopping__element">
+              Elemento lista de la compra
+            </li>
+          </ul>
+        </section>
+        <section className="section__pantry">
+          <h2 className="section__title">Tu despensa</h2>
+          <div>
+            <ul className="pantry__has">
+              <li className="patry__has--element">
+                <p>Alimento</p>
+                <span>Fecha de compra</span>
+                <button>Carro</button>
+                <button>Eliminar</button>
+              </li>
+            </ul>
+            <form className="pantry__form">
+              <input className="pantry__form--input" type="text" placeholder="Ingrediente" />
+              <input className="pantry__form--input" type="number" placeholder="Cantidad" />
+              <select className="pantry__form--input" name="unit" id="unit">
+                <option className="pantry__form--input" value="u">unidad</option>
+                <option className="pantry__form--input" value="kg">Kilogramos</option>
+                <option className="pantry__form--input" value="l">Litros</option>
+              </select>
+              <input className="pantry__form--input" type="date" name="date" id="date" placeholder="fecha de compra" />
+              <input className="pantry__form--input" type="submit" value="Añadir" />
+            </form>
+            <ul className="pantry__usually">
+              <li className="pantry__usually--element">
+                Ingredientes
+              </li>
+            </ul>
+          </div>
+          <h2 className="section__title">Añade un ingrediente</h2>
+          <h2 className="section__title">Ingredientes Habituales</h2>
+        </section>
+        <section className="section__planning">
+          <h2 className="section__title">Planning de comidas</h2>
+          <ol className="week">
+            <li className="week__day">
+              <p>Lunes</p>
+              <div>Comida</div>
+              <div>Cena</div>
+            </li>
+            <li className="week__day">
+              <p>Martes</p>
+              <div>Comida</div>
+              <div>Cena</div>
+            </li>
+            <li className="week__day">
+              <p>Miércoles</p>
+              <div>Comida</div>
+              <div>Cena</div>
+            </li>
+            <li className="week__day">
+              <p>Jueves</p>
+              <div>Comida</div>
+              <div>Cena</div>
+            </li>
+            <li className="week__day">
+              <p>Viernes</p>
+              <div>Comida</div>
+              <div>Cena</div>
+            </li>
+            <li className="week__day">
+              <p>Sábado</p>
+              <div>Comida</div>
+              <div>Cena</div>
+            </li>
+            <li className="week__day">
+              <p>Domingo</p>
+              <div>Comida</div>
+              <div>Cena</div>
+            </li>
+          </ol>
+          <h2 className="section__title">Recetas Sugeridas</h2>
+          <ul>
+            <li>
+              Receta
+            </li>
+          </ul>
+          <h2 className="section__title">Recetas favoritas</h2>
+          <ul>
+            <li>
+              Receta
+            </li>
+          </ul>
+          <h2 className="section__title">Recetas</h2>
+          <ul>
+            <li>
+              Receta
+            </li>
+          </ul>
+        </section>
+      </main>
+
       {/* <Switch>
         <Route path=''>
           //Contenido del componente correspondiente
